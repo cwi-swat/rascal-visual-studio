7513f73b1cf2a847cfddd498fd361b58139e5d05
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2023-01-30 11:51:44 +0100

    initial server setup; works on stdin/stdout

--- rascal-visual-studio-analysis-server/src/main/java/org/rascalmpl/visualstudio/analysis/server/Main.java
+++ rascal-visual-studio-analysis-server/src/main/java/org/rascalmpl/visualstudio/analysis/server/Main.java
@@ -1,9 +1,20 @@
 package org.rascalmpl.visualstudio.analysis.server;
 
+import org.eclipse.lsp4j.jsonrpc.Launcher;
+
 public class Main {
-    
 
     public static void main(String[] args) {
-        
+        IAnalysisServer server = new BasicAnalysisServer();
+
+        Launcher<IAnalysisServer> launch = new Launcher.Builder<IAnalysisServer>()
+            .setInput(System.in)
+            .setOutput(System.out)
+            .setLocalService(server)
+            .setRemoteInterface(IAnalysisServer.class)
+            .create();
+        ;
+
+        launch.startListening();
     }
 }
