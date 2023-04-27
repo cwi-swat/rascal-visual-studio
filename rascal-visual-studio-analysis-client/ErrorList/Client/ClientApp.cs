82735ad92e71f1f3df23c312b0eacf659b85f479
bsddemon <>
2023-04-27 09:23:49 +0200

    projects for experimenting with json-rpc

---
+++ rascal-visual-studio-analysis-client/ErrorList/Client/ClientApp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Threading;
+using StreamJsonRpc;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Client");
+            Process childProcess = Process.Start(new ProcessStartInfo("E:\\architectural-erosion\\rascal-visual-studio\\rascal-visual-studio-analysis-client\\ErrorList\\Server\\bin\\Debug\\Server.exe")
+            {
+                UseShellExecute = false,
+                RedirectStandardInput = true,
+                RedirectStandardOutput = true,
+            });
+            Console.ReadKey();
+        }
+    }
+}
---
+++ rascal-visual-studio-analysis-client/ErrorList/Server/ServerApp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Server");
+            Thread.Sleep(10000);
+        }
+    }
+}
