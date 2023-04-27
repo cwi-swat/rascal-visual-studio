3d114f4a2fe7550f8dae8c16f661f73b3dc1ace7
bsddemon <>
2023-04-27 09:42:55 +0200

    play with json-rpc

--- rascal-visual-studio-analysis-client/ErrorList/Client/ClientApp.cs
+++ rascal-visual-studio-analysis-client/ErrorList/Client/ClientApp.cs
@@ -1,25 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Text;
-using System.Windows.Controls;
-using System.Windows.Threading;
+using System.IO;
+using System.IO.Pipes;
+using System.Reflection;
+using System.Threading.Tasks;
+using Nerdbank.Streams;
 using StreamJsonRpc;
 
 namespace Server
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main()
         {
             Console.WriteLine("Client");
-            Process childProcess = Process.Start(new ProcessStartInfo("E:\\architectural-erosion\\rascal-visual-studio\\rascal-visual-studio-analysis-client\\ErrorList\\Server\\bin\\Debug\\Server.exe")
-            {
-                UseShellExecute = false,
-                RedirectStandardInput = true,
-                RedirectStandardOutput = true,
-            });
+            var childProcess = new ProcessStartInfo("E:\\architectural-erosion\\rascal-visual-studio\\rascal-visual-studio-analysis-client\\ErrorList\\Server\\bin\\Debug\\Server.exe", "");
+            childProcess.UseShellExecute = false;
+            childProcess.RedirectStandardInput = true;
+            childProcess.RedirectStandardOutput = true;
+            var process = Process.Start(childProcess);
+            var stdioStream = FullDuplexStream.Splice(process.StandardOutput.BaseStream, process.StandardInput.BaseStream);
+            await ActAsRpcClientAsync(stdioStream);
+            Console.WriteLine("Hit any key to stop the program");
             Console.ReadKey();
+        }
+
+        private static async Task ActAsRpcClientAsync(Stream stream)
+        {
+            Console.WriteLine("Connected. Sending request...");
+            var jsonRpc = JsonRpc.Attach(stream);
+            int sum = await jsonRpc.InvokeAsync<int>("Add", 3, 5);
+            Console.WriteLine($"3 + 5 = {sum}");
         }
     }
 }
--- rascal-visual-studio-analysis-client/ErrorList/Server/ServerApp.cs
+++ rascal-visual-studio-analysis-client/ErrorList/Server/ServerApp.cs
@@ -1,14 +1,37 @@
 ﻿using System;
-using System.Threading;
+using System.IO;
+using System.IO.Pipes;
+using System.Threading.Tasks;
+using Nerdbank.Streams;
+using StreamJsonRpc;
 
 namespace Server
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task<int> Main()
         {
-            Console.WriteLine("Server");
-            Thread.Sleep(10000);
+            await RespondToRpcRequestsAsync(FullDuplexStream.Splice(Console.OpenStandardInput(), Console.OpenStandardOutput()), 0);
+            return 0;
+        }
+
+        private static async Task RespondToRpcRequestsAsync(Stream stream, int clientId)
+        {
+            await Console.Error.WriteLineAsync($"Connection request #{clientId} received. Spinning off an async Task to cater to requests.");
+            var jsonRpc = JsonRpc.Attach(stream, new Server());
+            await Console.Error.WriteLineAsync($"JSON-RPC listener attached to #{clientId}. Waiting for requests...");
+            await jsonRpc.Completion;
+            await Console.Error.WriteLineAsync($"Connection #{clientId} terminated.");
+        }
+    }
+
+    internal class Server
+    {
+        public int Add(int a, int b)
+        {
+            // Log to STDERR so as to not corrupt the STDOUT pipe that we may be using for JSON-RPC.
+            Console.Error.WriteLine($"Received request: {a} + {b}");
+            return a + b;
         }
     }
 }
